f9e2d1654f0a32adca391c8f5a18308af6eb3cbe
Saulo Henrique Rodrigues <saulohrodrigues@gmail.com>
2024-02-18 14:19:14 -0300

    Subindo estrutura basica do projeto, construida com o spring initializr

---
+++ src/main/java/com/SpringQuiz/ProjetoSpringQuizApplication.java
@@ -0,0 +1,13 @@
+package com.SpringQuiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringQuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringQuizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SpringQuiz/ProjetoSpringQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringQuiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpringQuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
